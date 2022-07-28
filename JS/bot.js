f0d055f6e85537d750be31f3406e4b754ee9cafe
DamianEnderM <110199363+DamianEnderM@users.noreply.github.com>
2022-07-28 23:13:45 +0300

    bot.js
    my server

--- JS/bot.js
+++ JS/bot.js
@@ -4,8 +4,8 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "EDMgame.aternos.me",
+    port: "55608",
     username: "Test",
     version: false
     })
